da1d90c5578d18e7309ef51119e6d93050b8cfa4
Nataliya <Natalikristal@yandex.ru>
2026-09-17 10:31:28 +0300

    проект

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from '@playwright/test';
+
 export default defineConfig({
-  
   use: {
-    
     baseURL: 'https://natalikristal-188.130.251.61.sslip.io',
   },
+  
+  reporter: [['html', { open: 'always' }]], // <— Эта строчка открывает отчёт всегда
 });
